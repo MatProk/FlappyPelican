bbc1b688044e04abe61dffbf0d9670d62d593cda
Mati-PC\Mati <maciekprokop7@gmail.com>
2018-06-02 18:23:45 +0200

    [3]Changes made: -added game manager(switching between states)

--- FlappyMonkey/Assets/scripts/TapController.cs
+++ FlappyMonkey/Assets/scripts/TapController.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class TapController : MonoBehaviour {
+
+    public delegate void PlayerDelegate();
+    public static event PlayerDelegate OnPlayerDied;
+    public static event PlayerDelegate OnPlayerScored;
 
     public float tapForce = 10;
     public float tiltSmooth = 5;
@@ -13,13 +18,37 @@ public class TapController : MonoBehaviour {
     Quaternion downRotation;
     Quaternion forwardRotation;
 
+    GameManager game;
+
     void Start(){
         rigidbody = GetComponent<Rigidbody2D>();
         downRotation = Quaternion.Euler(0, 0, -90);
         forwardRotation = Quaternion.Euler(0, 0, 35);
+        game = GameManager.Instance;
+    }
+
+    void OnEnable(){
+        GameManager.OnGameStarted += OnGameStarted;
+        GameManager.OnGameOverConfirmed += OnGameOverConfirmed;
+    }
+
+    void OnDisable(){
+        GameManager.OnGameStarted -= OnGameStarted;
+        GameManager.OnGameOverConfirmed -= OnGameOverConfirmed;
+    }
+
+    void OnGameStarted(){
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.simulated = true;
+    }
+
+    void OnGameOverConfirmed(){
+        transform.localPosition = startPos;
+        transform.rotation = Quaternion.identity;
     }
 
     void Update(){
+        if (game.GameOver) return;
         if (Input.GetMouseButtonDown(0)){
             transform.rotation = forwardRotation;
             rigidbody.velocity = Vector3.zero;
@@ -32,12 +61,14 @@ public class TapController : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D col){
         if(col.gameObject.tag == "ScoreZone"){
             //register a score event
+            OnPlayerScored(); //event sent to GameManager
             //play a sound
         }
 
         if(col.gameObject.tag == "DeadZone"){
             rigidbody.simulated = false;
             //register a dead event
+            OnPlayerDied(); //event sent to GameManager
             //play a sound
         }
     }
